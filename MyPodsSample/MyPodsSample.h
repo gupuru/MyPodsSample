62869e44c0b4e3409bd094613edb21918794036a
gupuru <kouhei.aquamarines@gmail.com>
2016-02-22 17:13:09 +0900

    first commit

---
+++ MyPodsSample/MyPodsSample.h
@@ -0,0 +1,19 @@
+//
+//  MyPodsSample.h
+//  MyPodsSample
+//
+//  Created by 新見晃平 on 2016/02/22.
+//  Copyright © 2016年 kohei Niimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyPodsSample.
+FOUNDATION_EXPORT double MyPodsSampleVersionNumber;
+
+//! Project version string for MyPodsSample.
+FOUNDATION_EXPORT const unsigned char MyPodsSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPodsSample/PublicHeader.h>
+
+
---
+++ MyPodsSample/MyPodsSample.swift
@@ -0,0 +1,17 @@
+//
+//  MyPodsSample.swift
+//  MyPodsSample
+//
+//  Created by 新見晃平 on 2016/02/22.
+//  Copyright © 2016年 kohei Niimi. All rights reserved.
+//
+
+import Foundation
+
+public class MyPodsSample: NSObject{
+    
+    public class func start(){
+        print("podsのテストだよ")
+    }
+    
+}
